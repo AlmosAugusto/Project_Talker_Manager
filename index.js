728d0ece11358fe0ff8bb7de1ea16acbc0bf4cfe
almosAugusto <almos.filho@hotmail.com>
2022-05-03 12:42:19 -0300

    task 1 complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const fs = require('fs').promises;
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -10,6 +12,13 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// Task 1 - Crie o endpoint GET /talker
+
+app.get('/talker', async (_request, response) => {
+  const talker = await fs.readFile('./talker.json');
+  return response.status(200).json(JSON.parse(talker));
 });
 
 app.listen(PORT, () => {
